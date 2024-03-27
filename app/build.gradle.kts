09a4fdf3e0d29c288360708568c19e4120849d09
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2024-03-27 10:38:24 +0300

    add collapsing toolbar layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,7 +38,6 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -47,6 +46,7 @@ dependencies {
     implementation(libs.androidx.viewbinding)
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.androidx.coordinatorlayout)
     implementation(libs.dagger)
     kapt(libs.dagger.compiler)
     testImplementation(libs.junit)
